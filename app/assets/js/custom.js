48619e127a2e26ad71de33d84caca8ab6f813bc4
gcofficial <admin@gurievcreative.com>
2016-01-15 17:20:36 +0200

    Working on guidelines custom.js

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -28,87 +28,77 @@ function getWindowHeight() {
 
         var yPos = 0;
 
-        if (jQuery('.loader-wrapper').length > 0)
+        if ( jQuery( '.loader-wrapper' ).length > 0 )
         {
-            jQuery('.loader-wrapper').delay(1000).fadeOut();
+            jQuery( '.loader-wrapper' ).delay( 1000 ).fadeOut();
         }
 
-        if (!window.device.mobile() && !window.device.tablet() && !window.device.ipod()){
+        if ( ! window.device.mobile() && ! window.device.tablet() && ! window.device.ipod() ) {
 
-            if (window.photolab_custom.stickup_menu === '1')
+            if ( '1' === window.photolab_custom.stickup_menu )
             {
-                // sticky header
-                jQuery('.site-header').tmStickUp({
-                    correctionSelector: jQuery('#wpadminbar'),   
+
+                // Sticky header
+                jQuery( '.site-header' ).tmStickUp({
+                    correctionSelector: jQuery( '#wpadminbar' ),
                     active: true
                 });
             }
-            
-
-            // pages and posts header image parallax
-            $('.page-header-wrap').each(function(){
-                var coefficient = ( window.photolab_custom.stickup_menu !== '1' ) ? 6.3 : 1.7;
+            $( '.page-header-wrap' ).each( function() {
+                var coefficient = ( '1' !== window.photolab_custom.stickup_menu ) ? 6.3 : 1.7;
                 var $bgobj = $( this ).find( 'img' ),
-                    window_height = parseInt( getWindowHeight(), 10 ),
-                    element_pos = $bgobj.offset(),
-                    element_top = parseInt( element_pos.top, 10 ),
-                    buffer = Math.floor( element_top - window_height ),
-                    visible_scroll = parseInt( $( window ).scrollTop(), 10 ) - buffer;
-                if ( visible_scroll > 0 ) 
+                    coords = 0,
+                    windowHeight = parseInt( getWindowHeight(), 10 ),
+                    elementPos = $bgobj.offset(),
+                    elementTop = parseInt( elementPos.top, 10 ),
+                    buffer = Math.floor( elementTop - windowHeight ),
+                    visibleScroll = parseInt( $( window ).scrollTop(), 10 ) - buffer;
+                if ( visibleScroll > 0 )
                 {
-                    if ( window_height > element_top ) 
+                    if ( windowHeight > elementTop )
                     {
                         yPos = ( $( window ).scrollTop() / coefficient );
-                    } 
-                    else 
-                    {
-                        yPos = ( visible_scroll / coefficient );
+                    } else {
+                        yPos = ( visibleScroll / coefficient );
                     }
-                    var coords = yPos + 'px';
+                    coords = yPos + 'px';
                     $bgobj.css({ top: coords });
                 }
                 $( window ).scroll( function() {
-                    var element_pos = $bgobj.offset(),
-                        element_top = parseInt(element_pos.top, 10),
-                        //buffer = Math.floor(element_top / window_height);
-                        buffer = Math.floor(element_top - window_height),
-                        visible_scroll = parseInt($(window).scrollTop(), 10) - buffer;
-                   
-                    if ( visible_scroll > 0 ) {
-                        if ( window_height > element_top ) {
+                    var elementPos = $bgobj.offset(),
+                        coords = 0,
+                        elementTop = parseInt( elementPos.top, 10 ),
+                        buffer = Math.floor( elementTop - windowHeight ),
+                        visibleScroll = parseInt( $( window ).scrollTop(), 10 ) - buffer;
+                    if ( visibleScroll > 0 ) {
+                        if ( windowHeight > elementTop ) {
                             yPos = ( $( window ).scrollTop() / coefficient );
                         } else {
-                            yPos = ( visible_scroll / coefficient );
+                            yPos = ( visibleScroll / coefficient );
                         }
-                        var coords = yPos + 'px';
+                        coords = yPos + 'px';
                         $bgobj.css({ top: coords });
                     }
                 });
             });
-            
-            // home page header image parallax
-            $( '.home .header-image-box img' ).each( function(){
+            $( '.home .header-image-box img' ).each( function() {
 
-                var coefficient = ( window.photolab_custom.stickup_menu !== '1' ) ? 5.5 : 1.3;
-                var $bgobj = $(this),
-                    window_height = parseInt( getWindowHeight(), 10 ),
-                    element_pos = $bgobj.offset(),
-                    element_top = parseInt( element_pos.top, 10 ),
-                    //buffer = Math.floor(element_top / window_height);
-                    buffer = Math.floor( element_top - window_height ),
-                    visible_scroll = parseInt( $( window ).scrollTop(), 10 ) - buffer;
+                var coefficient = ( '1' !== window.photolab_custom.stickup_menu ) ? 5.5 : 1.3;
+                var $bgobj = $( this ),
+                    windowHeight = parseInt( getWindowHeight(), 10 ),
+                    elementPos = $bgobj.offset(),
+                    elementTop = parseInt( elementPos.top, 10 ),
+                    buffer = Math.floor( elementTop - windowHeight ),
+                    visibleScroll = parseInt( $( window ).scrollTop(), 10 ) - buffer;
 
-                if ( visible_scroll > 0) 
+                if ( visibleScroll > 0 )
                 {
-                    if ( window_height > element_top ) 
+                    if ( windowHeight > elementTop )
                     {
                         yPos = ( $( window ).scrollTop() / coefficient );
-                    } 
-                    else 
-                    {
-                        yPos = ( visible_scroll / coefficient );
+                    } else {
+                        yPos = ( visibleScroll / coefficient );
                     }
-                    //var coords = yPos + 'px';
                     $bgobj.css({
                         '-moz-transform': 'translateY(' + yPos + 'px)',
                         '-webkit-transform': 'translateY(' + yPos + 'px)',
@@ -118,19 +108,16 @@ function getWindowHeight() {
                     });
                 }
                 $( window ).scroll( function() {
-                    var element_pos = $bgobj.offset(),
-                        element_top = parseInt(element_pos.top, 10),
-                        //buffer = Math.floor(element_top / window_height);
-                        buffer = Math.floor( element_top - window_height ),
-                        visible_scroll = parseInt( $( window ).scrollTop(), 10 ) - buffer;
-                   
-                    if ( visible_scroll > 0) {
-                        if ( window_height > element_top ) {
+                    var elementPos = $bgobj.offset(),
+                        elementTop = parseInt( elementPos.top, 10 ),
+                        buffer = Math.floor( elementTop - windowHeight ),
+                        visibleScroll = parseInt( $( window ).scrollTop(), 10 ) - buffer;
+                    if ( visibleScroll > 0 ) {
+                        if ( windowHeight > elementTop ) {
                             yPos = ( $( window ).scrollTop() / coefficient );
                         } else {
-                            yPos = ( visible_scroll / coefficient );
+                            yPos = ( visibleScroll / coefficient );
                         }
-                        //var coords = yPos + 'px';
                         $bgobj.css({
                             '-moz-transform': 'translateY(' + yPos + 'px)',
                             '-webkit-transform': 'translateY(' + yPos + 'px)',
@@ -143,94 +130,100 @@ function getWindowHeight() {
             });
         }
     });
-    
-    // init single popup
-	$( function(){
-		$( '.lightbox-image a' ).magnificPopup({ 
-			type: 'image',
-			mainClass: 'mfp-with-zoom', // this class is for CSS animation below
 
-			zoom: {
-				enabled: true, // By default it's false, so don't forget to enable it
+    // Init single popup
+    $( function() {
+        $( '.lightbox-image a' ).magnificPopup({
+            type: 'image',
+            mainClass: 'mfp-with-zoom', // This class is for CSS animation below
 
-				duration: 300, // duration of the effect, in milliseconds
-				easing: 'ease-in-out', // CSS transition easing function 
+            zoom: {
+                enabled: true, // By default it's false, so don't forget to enable it
 
-			opener: function( openerElement ) {
-		  		return openerElement.is( 'img' ) ? openerElement : openerElement.find( 'img' );
-			}
-		}
+                duration: 300, // Duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function
 
-		});
-	});
+            opener: function( openerElement ) {
+                return openerElement.is( 'img' ) ? openerElement : openerElement.find( 'img' );
+            }
+        }
 
-    jQuery( document ).ready( function($) {
+        });
+    });
+
+    jQuery( document ).ready( function( $ ) {
         var $container = $( '#masonry' );
         $container.masonry({
            itemSelector: '.brick'
         });
-        // init popup galleries for gallery post format featured galleries
+
+        // Init popup galleries for gallery post format featured galleries
         $( '.post-featured-gallery' ).each( function() {
             $( '#' + $( this ).data( 'gall-id' ) + ' .lightbox-gallery' ).magnificPopup({
                 type: 'image',
                 gallery:{
                     enabled:true
                 },
-                mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+                mainClass: 'mfp-with-zoom', // This class is for CSS animation below
 
                 zoom: {
                     enabled: true, // By default it's false, so don't forget to enable it
 
-                    duration: 300, // duration of the effect, in milliseconds
-                    easing: 'ease-in-out', // CSS transition easing function 
+                    duration: 300, // Duration of the effect, in milliseconds
+                    easing: 'ease-in-out', // CSS transition easing function
 
-                    // The 'opener' function should return the element from which popup will be zoomed in
-                    // and to which popup will be scaled down
-                    // By defailt it looks for an image tag:
+                    /**
+                     * The 'opener' function should return the element from which popup will be zoomed in
+                     * and to which popup will be scaled down
+                     * By defailt it looks for an image tag:
+                     */
                     opener: function( openerElement ) {
-                        // openerElement is the element on which popup was initialized, in this case its <a> tag
-                        // you don't need to add 'opener' option if this code matches your needs, it's defailt one.
-                        return openerElement.is('img') ? openerElement : openerElement.find('img');
+                        /**
+                         * OpenerElement is the element on which popup was initialized, in this case its <a> tag
+                         * you don't need to add 'opener' option if this code matches your needs, it's defailt one.
+                         */
+                        return openerElement.is( 'img' ) ? openerElement : openerElement.find( 'img' );
                     }
                 }
-            }); 
+            });
         });
     });
 
-    // to top button
-    jQuery( window ).scroll( function () {
+    // To top button
+    jQuery( window ).scroll( function() {
         if ( jQuery( this ).scrollTop() > 100 ) {
             jQuery( '#back-top' ).fadeIn();
         } else {
             jQuery( '#back-top' ).fadeOut();
         }
     });
-    
-    jQuery( '#back-top a' ).click( function () {
+
+    jQuery( '#back-top a' ).click( function() {
         jQuery( 'body,html' ).stop( false, false ).animate({
             scrollTop: 0
         }, 800 );
         return false;
     });
 
-    // dropdown menu and mobile navigation
-    jQuery( document ).ready( function($) {
+    // Dropdown menu and mobile navigation
+    jQuery( document ).ready( function( $ ) {
+        var ismobile = false;
         $( 'ul.sf-menu' ).superfish();
 
-        var ismobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
-        if(ismobile){
-            jQuery('.main-navigation > ul').sftouchscreen();
+        ismobile = navigator.userAgent.match( /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i );
+        if ( ismobile ) {
+            jQuery( '.main-navigation > ul' ).sftouchscreen();
         }
 
-        jQuery('.main-navigation > ul').mobileMenu();
+        jQuery( '.main-navigation > ul' ).mobileMenu();
     });
 
-})(jQuery);
+})( jQuery );
 
-jQuery(document).on(
+jQuery( document ).on(
     'click',
     '#top-bar-search-button',
-    function(e){
+    function( e ) {
         e.preventDefault();
     }
 );
@@ -238,8 +231,8 @@ jQuery(document).on(
 jQuery( '#top-bar-search-button' ).on({
     focus: function() {
         jQuery( '#top-bar-search-form' ).parent().addClass( 'adminbar-focused' );
-    }, 
-    blur: function() 
+    },
+    blur: function()
     {
         jQuery( '#top-bar-search-form' ).parent().removeClass( 'adminbar-focused' );
     }
