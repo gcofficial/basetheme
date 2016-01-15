6b19e23a62e0560fb13ae29c867f451ce9708252
gcofficial <admin@gurievcreative.com>
2016-01-15 17:56:44 +0200

    Working on guidelines skip-link-focus-fix.js

--- app/assets/js/skip-link-focus-fix.js
+++ app/assets/js/skip-link-focus-fix.js
@@ -1,24 +1,24 @@
 /**
- * skip-link-focus-fix.js
+ * Skip-link-focus-fix.js
  * licensed under GNU General Public License v3
  */
 ( function() {
-	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
-	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+    var isWebkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+        isOpera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+        isIe     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
 
-	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
-		window.addEventListener( 'hashchange', function() {
-			var element = document.getElementById( location.hash.substring( 1 ) );
+    if ( ( isWebkit || isOpera || isIe ) && document.getElementById && window.addEventListener ) {
+        window.addEventListener( 'hashchange', function() {
+            var element = document.getElementById( location.hash.substring( 1 ) );
 
-			if ( element ) {
-				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
-				{
-					element.tabIndex = -1;
-				}
+            if ( element ) {
+                if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+                {
+                    element.tabIndex = -1;
+                }
 
-				element.focus();
-			}
-		}, false );
-	}
+                element.focus();
+            }
+        }, false );
+    }
 })();
