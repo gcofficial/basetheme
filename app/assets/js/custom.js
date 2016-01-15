13a4da5f1c1dc252d4aca1c49759c6aefe93d71c
gcofficial <admin@gurievcreative.com>
2016-01-15 14:32:54 +0200

    Working on guidlines

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -5,13 +5,18 @@
 
 function getWindowHeight() {
     var myWidth = 0, myHeight = 0;
-    if( typeof( window.innerWidth ) == 'number' ) {
+    if( typeof( window.innerWidth ) === 'number' ) 
+    {
         //Non-IE
         myHeight = window.innerHeight;
-    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+    } 
+    else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) 
+    {
         //IE 6+ in 'standards compliant mode'
         myHeight = document.documentElement.clientHeight;
-    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+    } 
+    else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) 
+    {
         //IE 4 compatible
         myHeight = document.body.clientHeight;
     }
