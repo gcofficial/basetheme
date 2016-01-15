b6f23cf1e53ec5f7b0cec3ec7b2f6ca1e0f1b82e
gcofficial <admin@gurievcreative.com>
2016-01-15 17:29:41 +0200

    Working on guidelines customiser.js

--- app/assets/js/customizer.js
+++ app/assets/js/customizer.js
@@ -6,94 +6,92 @@
  */
 function ctrlToggle( $ctrl, show )
 {
-	if ( show )
-	{
-		$ctrl.show();
-	}
-	else	
-	{
-		$ctrl.hide();
-	}
+    if ( show )
+    {
+        $ctrl.show();
+    } else {
+        $ctrl.hide();
+    }
 }
 
 /**
  * Change top menu location
  */
 jQuery( document ).on(
-	'change',
-	'#customize-control-nav_menu_locations-top select',
-	function(){
-		var val = jQuery( this ).val();
+    'change',
+    '#customize-control-nav_menu_locations-top select',
+    function() {
+        var val = jQuery( this ).val();
 
-		ctrlToggle(
-			jQuery('#customize-control-disclimer_text'),
-			val !== 0
-		);
-		ctrlToggle(
-			jQuery('#customize-control-search_box'),
-			val !== 0
-		);
-	}
+        ctrlToggle(
+            jQuery( '#customize-control-disclimer_text' ),
+            0 !== val
+        );
+        ctrlToggle(
+            jQuery( '#customize-control-search_box' ),
+            0 !== val
+        );
+    }
 );
 
 /**
  * Change layout style
  */
 jQuery( document ).on(
-	'change',
-	'#customize-control-layout_style select',
-	function(){
-		var val = jQuery( this ).val();
+    'change',
+    '#customize-control-layout_style select',
+    function() {
+        var val = jQuery( this ).val();
 
-		ctrlToggle(
-			jQuery('#customize-control-columns'),
-			val === 'grid' || val === 'masonry'
-		);
-	}
+        ctrlToggle(
+            jQuery( '#customize-control-columns' ),
+            'grid' === val || 'masonry' === val
+        );
+    }
 );
 
 /**
  * Change footer style
  */
 jQuery( document ).on(
-	'change',
-	'#customize-control-footer_style select',
-	function(){
-		var val = jQuery( this ).val();
+    'change',
+    '#customize-control-footer_style select',
+    function() {
+        var val = jQuery( this ).val();
 
-		ctrlToggle( jQuery('#customize-control-footer_logo' ), val === 'centered');
-		ctrlToggle( jQuery('#customize-control-footer_columns' ), val === 'default' || val === 'centered');
-	}
+        ctrlToggle( jQuery( '#customize-control-footer_logo' ), 'centered' === val );
+        ctrlToggle( jQuery( '#customize-control-footer_columns' ), 'default' === val || 'centered' === val );
+    }
 );
 
 /**
  * Document ready
  */
 jQuery( document ).ready(
-	function(){
-		var footer_style = jQuery('#customize-control-footer_style select').val();
-		var layout_style = jQuery('#customize-control-layout_style select').val();
-		var top_menu     = jQuery('#customize-control-nav_menu_locations-top select').val();
+    function() {
+        var footerStyle = jQuery( '#customize-control-footer_style select' ).val();
+        var layoutStyle = jQuery( '#customize-control-layout_style select' ).val();
+        var topMenu     = jQuery( '#customize-control-nav_menu_locations-top select' ).val();
 
-		ctrlToggle(
-			jQuery( '#customize-control-footer_logo' ), 
-			footer_style === 'centered'
-		);
-		ctrlToggle(
-			jQuery( '#customize-control-footer_columns' ), 
-			footer_style === 'default' || footer_style === 'centered'
-		);	
-		ctrlToggle(
-			jQuery( '#customize-control-columns' ),
-			layout_style === 'grid' || layout_style === 'masonry'
-		);
-		ctrlToggle(
-			jQuery( '#customize-control-disclimer_text' ),
-			top_menu !== 0
-		);
-		ctrlToggle(
-			jQuery( '#customize-control-search_box' ),
-			top_menu !== 0
-		);
-	}
+        ctrlToggle(
+            jQuery( '#customize-control-footer_logo' ),
+            'centered' === footerStyle
+        );
+        ctrlToggle(
+            jQuery( '#customize-control-footer_columns' ),
+            'default' === footerStyle || 'centered' === footerStyle
+        );
+        ctrlToggle(
+            jQuery( '#customize-control-columns' ),
+            'grid' === layoutStyle || 'masonry' === layoutStyle
+        );
+        ctrlToggle(
+            jQuery( '#customize-control-disclimer_text' ),
+            0 !== topMenu
+        );
+        ctrlToggle(
+            jQuery( '#customize-control-search_box' ),
+            0 !== topMenu
+        );
+    }
 );
