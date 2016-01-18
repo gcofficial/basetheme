ce9da98b4332ba8af6f4065f0eb06b7d7c4ce352
gcofficial <admin@gurievcreative.com>
2016-01-18 11:38:18 +0200

    phpcs app/models/header-settings.php

--- app/models/header-settings.php
+++ app/models/header-settings.php
@@ -1,71 +1,73 @@
 <?php
+/**
+ * Header settings model file
+ *
+ * @package photolab
+ */
 
+/**
+ * Header setting model class
+ */
 class Header_Settings_Model extends Options_Model{
 
 	/**
 	 * Get all options
 	 * @return array --- all options
 	 */
-	public static function getAll()
-	{
-		return (array) get_option('header_settings');
+	public static function getAll() {
+		return (array) get_option( 'header_settings' );
 	}
 
 	/**
 	 * Get stickup menu checkbox value
 	 * @return boolean --- true or false
 	 */
-	public static function getStickupMenu()
-	{
-		return (bool) self::getOption('stickup_menu');
+	public static function getStickupMenu() {
+		return (bool) self::getOption( 'stickup_menu' );
 	}
 
 	/**
 	 * Get show title on header checkbox value
 	 * @return boolean --- true or false
 	 */
-	public static function is_show_title_on_header()
-	{
-		return (bool) self::getOption('show_title_on_header');
+	public static function is_show_title_on_header() {
+		return (bool) self::getOption( 'show_title_on_header' );
 	}
 
 	/**
 	 * Get enable/disable flag title attributes
 	 * @return boolean --- enabled or disabled
 	 */
-	public static function getTitleAttributes()
-	{
-		return (bool) self::getOption('title_attributes');
+	public static function getTitleAttributes() {
+		return (bool) self::getOption( 'title_attributes' );
 	}
 
 	/**
 	 * Get enable/disable flag search box
 	 * @return boolean --- enabled or disabled
 	 */
-	public static function getSearchBox()
-	{
-		return (bool) self::getOption('search_box');
+	public static function getSearchBox() {
+		return (bool) self::getOption( 'search_box' );
 	}
 
 	/**
 	 * Get disclimer text HTML code
 	 * @return string --- disclimer text HTML code
 	 */
-	public static function getDisclimer()
-	{
-		return (string) self::getOption('disclimer_text');
+	public static function getDisclimer() {
+		return (string) self::getOption( 'disclimer_text' );
 	}
 
 	/**
 	 * Get current header style
 	 * @return string --- header style
 	 */
-	public static function getHeaderStyle()
-	{
+	public static function getHeaderStyle() {
 		$allowed_header_styles = self::getAllowedHeaderStyles();
-		$header_style = (string) self::getOption('header_style');
-		if(in_array($header_style, $allowed_header_styles))
+		$header_style = (string) self::getOption( 'header_style' );
+		if ( in_array( $header_style, $allowed_header_styles ) ) {
 			return $header_style;
+		}
 		return $allowed_header_styles[0];
 	}
 
@@ -73,48 +75,44 @@ class Header_Settings_Model extends Options_Model{
 	 * Get all allowed header styles
 	 * @return array --- all allowed header sytles
 	 */
-	public static function getAllowedHeaderStyles()
-	{
-		return array(
+	public static function getAllowedHeaderStyles() {
+		return [
 			'default',
 			'minimal',
-			'centered'
-		);
+			'centered',
+		];
 	}
 
 	/**
 	 * Get header css classes
-	 * 
+	 *
 	 * @return string css classes
 	 */
-	public static function getHeaderClass()
-	{
-		$classes = array(
+	public static function getHeaderClass() {
+		$classes = [
 			'page-header',
-			'header-type-'.rand(1, 8),
-		);
+			'header-type-' . rand( 1, 8 ),
+		];
 
-		if(!Header_Settings_Model::is_show_title_on_header())
-		{
-			array_push($classes, 'invisibility');
+		if ( ! Header_Settings_Model::is_show_title_on_header() ) {
+			array_push( $classes, 'invisibility' );
 		}
-		if(has_post_thumbnail() OR get_header_image())
-		{
-			array_push($classes, 'with-img');
+		if ( has_post_thumbnail() or get_header_image() ) {
+			array_push( $classes, 'with-img' );
 		}
-		return implode(' ', $classes);
+		return implode( ' ', $classes );
 	}
 
 	/**
 	 * Add title to content
-	 * @param string $content --- html code
+	 * @param string $content html code.
 	 */
-	public static function add_title_to_content( $content ) 
-	{
+	public static function add_title_to_content( $content ) {
 		$custom_content = '';
-		if(!self::is_show_title_on_header())	
-	    	$custom_content = renderTitle();
-	    $custom_content .= $content;
-	    return $custom_content;
+		if ( ! self::is_show_title_on_header() ) {
+			$custom_content = renderTitle();
+		}
+		$custom_content .= $content;
+		return $custom_content;
 	}
 }
