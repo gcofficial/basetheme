b3ee8303d6f0ee68d33bad69ad05b8a4a00283ff
gcofficial <admin@gurievcreative.com>
2016-01-18 08:24:45 +0200

    Working on guidelines utilits.js

--- app/assets/js/utilits.js
+++ app/assets/js/utilits.js
@@ -1,14 +1,12 @@
-function wheel(event) {
-    if (event.wheelDelta) 
+function wheel( event ) {
+    if ( event.wheelDelta )
     {
         window.delta = event.wheelDelta / 90;
-    }
-    else if (event.detail)
-    {
+    } else if ( event.detail ) {
          window.delta = -event.detail / 3;
     }
     handle();
-    if (event.preventDefault) 
+    if ( event.preventDefault )
     {
         event.preventDefault();
     }
@@ -18,56 +16,58 @@ function wheel(event) {
 function handle() {
     var time = 330;
     var distance = 100;
-    jQuery('html, body').stop().animate({
-        scrollTop: jQuery(window).scrollTop() - (distance * window.delta)
-    }, time);
+    jQuery( 'html, body' ).stop().animate( {
+        scrollTop: jQuery( window ).scrollTop() - ( distance * window.delta )
+    }, time );
 }
-jQuery(window).load(function(){  
-    if(checkBrowser() === 'Google Chrome'){
-        if (window.addEventListener) 
+jQuery( window ).load( function() {
+    if ( 'Google Chrome' === checkBrowser() )
+    {
+        if ( window.addEventListener )
         {
-            window.addEventListener('DOMMouseScroll', wheel, false);
+            window.addEventListener( 'DOMMouseScroll', wheel, false );
         }
 
         window.onmousewheel = document.onmousewheel = wheel;
     }
 });
-function checkBrowser(){
+function checkBrowser()
+{
     var ua = navigator.userAgent;
-    
-    if (ua.search(/MSIE/) > 0) 
+
+    if ( ua.search( /MSIE/ ) > 0 )
     {
         return 'Internet Explorer';
     }
-    if (ua.search(/Firefox/) > 0) 
+    if ( ua.search( /Firefox/ ) > 0 )
     {
         return 'Firefox';
     }
-    if (ua.search(/Opera/) > 0) 
+    if ( ua.search( /Opera/ ) > 0 )
     {
         return 'Opera';
     }
-    if (ua.search(/Chrome/) > 0) 
+    if ( ua.search( /Chrome/ ) > 0 )
     {
         return 'Google Chrome';
     }
-    if (ua.search(/Safari/) > 0) 
+    if ( ua.search( /Safari/ ) > 0 )
     {
         return 'Safari';
     }
-    if (ua.search(/Konqueror/) > 0) 
+    if ( ua.search( /Konqueror/ ) > 0 )
     {
         return 'Konqueror';
     }
-    if (ua.search(/Iceweasel/) > 0) 
+    if ( ua.search( /Iceweasel/ ) > 0 )
     {
         return 'Debian Iceweasel';
     }
-    if (ua.search(/SeaMonkey/) > 0) 
+    if ( ua.search( /SeaMonkey/ ) > 0 )
     {
         return 'SeaMonkey';
     }
-    if (ua.search(/Gecko/) > 0) 
+    if ( ua.search( /Gecko/ ) > 0 )
     {
         return 'Gecko';
     }
