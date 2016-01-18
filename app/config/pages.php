7da1945ea8d83e0d14a3ba42a0ef1a3e0a216a4b
gcofficial <admin@gurievcreative.com>
2016-01-18 10:05:56 +0200

    phpcs app/config/pages.php

--- app/config/pages.php
+++ app/config/pages.php
@@ -6,21 +6,12 @@
  */
 return [
 	'Upgrade_To_Pro' => [
-		__('About Photolab', 'photolab'), 
-		__('About Photolab', 'photolab'), 
-		'edit_theme_options', 
-		'welcome.php', 
+		__( 'About Photolab', 'photolab' ),
+		__( 'About Photolab', 'photolab' ),
+		'edit_theme_options',
+		'welcome.php',
 		function(){
-			echo \View\View::make('pages/about_photolab');
-		}
+			echo \View\View::make( 'pages/about_photolab' );
+		},
 	],
-	'Fuck_the_planet' => [
-		__('Example', 'photolab'), 
-		__('Example', 'photolab'), 
-		'edit_theme_options', 
-		'fuck_the_planet.php', 
-		function(){
-			echo \View\View::make('pages/fuck_the_planet', ['fuck_variable' => 'Fuck the planet']);
-		}
-	]
 ];
