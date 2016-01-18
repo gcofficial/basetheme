1bc59ef60579dc5dcca8fad0f33ade32c8c064bf
gcofficial <admin@gurievcreative.com>
2016-01-18 12:27:59 +0200

    phpcs app/models/social-setting.php

--- app/models/social-settings.php
+++ app/models/social-settings.php
@@ -1,55 +1,57 @@
 <?php
+/**
+ * Social settings model file
+ *
+ * @package photolab
+ */
 
+/**
+ * Social settings model class
+ */
 class Social_Settings_Model extends Options_Model{
 
 	/**
 	 * Get all options
 	 * @return array --- all options
 	 */
-	public static function get_all()
-	{
-		return (array) get_option('social_settings');
+	public static function get_all() {
+		return (array) get_option( 'social_settings' );
 	}
 
 	/**
 	 * Show socials on header
-	 * 
+	 *
 	 * @return boolean
 	 */
-	public static function is_show_header()
-	{
-		return self::get_option('header') != '';
+	public static function is_show_header() {
+		return '' != self::get_option( 'header' );
 	}
 
 	/**
 	 * Show socials on footer
-	 * 
+	 *
 	 * @return boolean
 	 */
-	public static function is_show_footer()
-	{
-		return self::get_option('footer') != '';
+	public static function is_show_footer() {
+		return '' != self::get_option( 'footer' );
 	}
 
 	/**
 	 * Get all social with URLS
-	 * 
+	 *
 	 * @return array
 	 */
-	public static function get_all_socials()
-	{
+	public static function get_all_socials() {
 		$allowed     = self::get_allowed();
 		$social_urls = [];
 
-		foreach ($allowed as $key => $properties) 
-		{
-			$option = (string) self::get_option(sprintf('%s_url', $key));
-			$option = trim($option);
+		foreach ( $allowed as $key => $properties ) {
+			$option = (string) self::get_option( sprintf( '%s_url', $key ) );
+			$option = trim( $option );
 
-			if($option != '')
-			{
-				$properties['url'] = $option;
-				$social_urls[$key] = $properties;
+			if ( '' != $option ) {
+				$properties['url']   = $option;
+				$social_urls[ $key ] = $properties;
 			}
 		}
 		return $social_urls;
@@ -57,40 +59,39 @@ class Social_Settings_Model extends Options_Model{
 
 	/**
 	 * Get all allowed socials
-	 * 
+	 *
 	 * @return array
 	 */
-	public static function get_allowed()
-	{
+	public static function get_allowed() {
 		return [
 			'facebook' => [
 				'label' => __( 'Facebook', 'photolab' ),
-				'icon'  => 'fa-facebook-official'
+				'icon'  => 'fa-facebook-official',
 			],
 			'twitter' => [
 				'label' => __( 'Twitter', 'photolab' ),
-				'icon'  => 'fa-twitter'
+				'icon'  => 'fa-twitter',
 			],
 			'google-plus' => [
 				'label' => __( 'Google+', 'photolab' ),
-				'icon'  => 'fa-google-plus'
+				'icon'  => 'fa-google-plus',
 			],
 			'instagram' => [
 				'label' => __( 'Instagram', 'photolab' ),
-				'icon'  => 'fa-instagram'
+				'icon'  => 'fa-instagram',
 			],
 			'linkedin' => [
 				'label' => __( 'LinkedIn', 'photolab' ),
-				'icon'  => 'fa-linkedin'
+				'icon'  => 'fa-linkedin',
 			],
 			'dribbble' => [
 				'label' => __( 'Dribbble', 'photolab' ),
-				'icon'  => 'fa-dribbble'
+				'icon'  => 'fa-dribbble',
 			],
 			'youtube' => [
 				'label' => __( 'YouTube', 'photolab' ),
-				'icon'  => 'fa-youtube'
-			]
+				'icon'  => 'fa-youtube',
+			],
 		];
 	}
 }
