61edbb1f509cd43a7f535f489038baf12ac9885f
gcofficial <admin@gurievcreative.com>
2016-01-18 12:05:07 +0200

    phpcs apps/models/misc.php

--- app/models/misc.php
+++ app/models/misc.php
@@ -1,75 +1,76 @@
 <?php
+/**
+ * Misc model file
+ *
+ * @package photolab
+ */
 
+/**
+ * Mis model class
+ */
 class Misc_Model extends Options_Model{
 
 	/**
 	 * Get all options
-	 * 
+	 *
 	 * @return array --- all options
 	 */
-	public static function getAll()
-	{
-		return (array) get_option('misc');
+	public static function getAll() {
+		return (array) get_option( 'misc' );
 	}
 
 	/**
 	 * Get featured label
-	 * 
+	 *
 	 * @return string
 	 */
-	public static function getFeaturedLabel()
-	{
-		return (string) self::getOption('featured_label');
+	public static function getFeaturedLabel() {
+		return (string) self::getOption( 'featured_label' );
 	}
 
 	/**
 	 * Get blog label
-	 * 
+	 *
 	 * @return string
 	 */
-	public static function getBlogLabel()
-	{
-		return (string) self::getOption('blog_label');
+	public static function getBlogLabel() {
+		return (string) self::getOption( 'blog_label' );
 	}
 
 	/**
 	 * Get blog content
-	 * 
+	 *
 	 * @return string
 	 */
-	public static function getBlogContent()
-	{
-		$allowed = array('excerpt', 'full');
-		$content = (string) self::getOption('blog_content');
-		if(in_array($content, $allowed))
+	public static function getBlogContent() {
+		$allowed = [ 'excerpt', 'full' ];
+		$content = (string) self::getOption( 'blog_content' );
+		if ( in_array( $content, $allowed ) ) {
 			return $content;
+		}
 		return $allowed[0];
 	}
 
 	/**
 	 * Get blog image
-	 * 
+	 *
 	 * @return string
 	 */
-	public static function getBlogImage()
-	{
-		$blog_image = (string) self::getOption('blog_image');
-		return $blog_image != '' ? $blog_image : 'post-thumbnail';
+	public static function getBlogImage() {
+		$blog_image = (string) self::getOption( 'blog_image' );
+		return '' != $blog_image ? $blog_image : 'post-thumbnail';
 	}
 
 	/**
 	 * Get blog button
-	 * 
+	 *
 	 * @return string
 	 */
-	public static function getBlogButton()
-	{
-		$blog_btn = (string) self::getOption('blog_btn');
-		if($blog_btn == '')
-		{
-			$blog_btn = __( "Read More", "photolab" );	
+	public static function getBlogButton() {
+		$blog_btn = (string) self::getOption( 'blog_btn' );
+		if ( '' == $blog_btn ) {
+			$blog_btn = __( 'Read More', 'photolab' );
 		}
 		return $blog_btn;
 	}
-	
 }
