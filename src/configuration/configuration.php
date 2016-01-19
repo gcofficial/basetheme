a13d128bdc1fbf1d7cf51f0883995c58927c9a7f
gcofficial <admin@gurievcreative.com>
2016-01-19 12:47:13 +0200

    phpcs src/configuration/configuration.php

--- src/configuration/configuration.php
+++ src/configuration/configuration.php
@@ -1,33 +1,37 @@
 <?php
+/**
+ * Configuration engine class file
+ *
+ * @package photolab
+ */
 
 namespace Configuration;
 
-class Configuration{
+/**
+ * Configuration class
+ */
+class Configuration {
 
 	/**
 	 * Load all configurations
 	 */
-	public static function load()
-	{
+	public static function load() {
 		$allowed_classes = self::get_allowed_config_classes();
-		foreach ($allowed_classes as $class_name) 
-		{
-			$path  = self::get_class_path($class_name);
-			$class = sprintf('\\Configuration\\%s', $class_name);
-			if(file_exists($path))
-			{
-				new $class(include($path));
+		foreach ( $allowed_classes as $class_name ) {
+			$path  = self::get_class_path( $class_name );
+			$class = sprintf( '\\Configuration\\%s', $class_name );
+			if ( file_exists( $path ) ) {
+				new $class( include( $path ) );
 			}
 		}
 	}
 
 	/**
 	 * Get all allowed config classes
-	 * 
+	 *
 	 * @return array all allowed config classes
 	 */
-	public static function get_allowed_config_classes()
-	{
+	public static function get_allowed_config_classes() {
 		return array(
 			'Models',
 			'Images',
@@ -43,16 +47,15 @@ class Configuration{
 
 	/**
 	 * Make class path from class name
-	 * 
-	 * @param type $class_name 
+	 *
+	 * @param type $class_name class name.
 	 * @return string class path
 	 */
-	public static function get_class_path($class_name)
-	{
+	public static function get_class_path( $class_name ) {
 		return sprintf(
-			'%s/app/config/%s.php', 
+			'%s/app/config/%s.php',
 			get_template_directory(),
-			strtolower($class_name)
+			strtolower( $class_name )
 		);
 	}
 }
