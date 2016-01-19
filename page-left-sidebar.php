f6409215ba1661357971dd74241d1d5b3943784a
gcofficial <admin@gurievcreative.com>
2016-01-19 11:56:15 +0200

    phpcs page-left-sidebar and page-right-sidebar

--- page-left-sidebar.php
+++ page-left-sidebar.php
@@ -11,6 +11,6 @@ echo \View\View::make(
 	[
 		'sidebar_left'      => Sidebar_Settings_Model::getModeLeft(),
 		'breadcrumbs'       => General_Site_Settings_Model::breadcrumbs(),
-		'sidebar_side_type' => 'left'
+		'sidebar_side_type' => 'left',
 	]
 );
--- page-right-sidebar.php
+++ page-right-sidebar.php
@@ -11,7 +11,7 @@ echo \View\View::make(
 	[
 		'sidebar_right'     => Sidebar_Settings_Model::getModeRight(),
 		'breadcrumbs'       => General_Site_Settings_Model::breadcrumbs(),
-		'sidebar_side_type' => 'right'
+		'sidebar_side_type' => 'right',
 	]
 );
 
