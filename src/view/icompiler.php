c6f7cfa2b5cad64339afab178fe12bcfcab9ff09
gcofficial <admin@gurievcreative.com>
2016-01-19 15:23:45 +0200

    phpcs src/view/scout-compiler.php

--- src/view/icompiler.php
+++ src/view/icompiler.php
@@ -1,14 +1,22 @@
 <?php
+/**
+ * Core ICompiler interface file
+ *
+ * @package photolab
+ */
+
 namespace View;
 
+/**
+ * ICompiler interfac
+ */
 interface ICompiler {
 
-    /**
-     * Compile the view at the given path.
-     *
-     * @param type $path
-     * @return void
-     */
-    public function compile($path);
-
-} 
+	/**
+	 * Compile the view at the given path.
+	 *
+	 * @param type $path compile path.
+	 * @return void
+	 */
+	public function compile( $path );
+}
