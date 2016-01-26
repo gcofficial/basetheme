78c3c27221b00bc40bef6fefefd5794935c22bb2
RDSergij <sergijlys@gmail.com>
2016-01-26 11:31:43 +0200

    Fix href

--- app/views/widgets/front-end/twitter-timeline.php
+++ app/views/widgets/front-end/twitter-timeline.php
@@ -10,10 +10,8 @@
 	{{ $before_title }}{{ apply_filters( 'widget_title', $title ) }}{{ $after_title }}
 @endif
 <div class="tm-twitter-timeline-widget">
-	<a class="twitter-timeline"
-	  data-widget-id="{{ $widget_id }}"
-	  href="https://twitter.com/{{ $screen_name }}"
-	  data-screen-name="{{ $screen_name }}">
+	<a class="twitter-timeline" data-widget-id="{{ $widget_id }}" href="https://twitter.com/{{ $screen_name }}" data-screen-name="{{ $screen_name }}">
 	Tweets by @{{ $screen_name }}
+	</a>
 </div>
 {{ $after_widget }}
