f99d76263a79c7ea2a5c67bea2295a0b2d785d1d
gcofficial <tatarin@Gurievs-Mac-mini.local>
2016-01-28 14:03:23 +0200

    Added Model_Logo_And_FavIcon

---
+++ app/models/model-logo-and-favicon.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * General Site settings model
+ *
+ * @package photolab
+ */
+
+class Model_Logo_And_Favicon{
+	/**
+	 * Get single option by key
+	 *
+	 * @return mixed --- option type.
+	 */
+	public static function get_option( $key ) {
+		$logo_and_favicon = Utils::array_get(
+			(array) get_option( 'general_site_settings' ),
+			'logo_and_favicon',
+			array( 'logo_and_favicon' => array() )
+		);
+		return Utils::array_get( (array) $logo_and_favicon, $key, '' );
+	}
+
+	/**
+	 * Is enable page preloader ?
+	 *
+	 * @return boolean Enable / Disable.
+	 */
+	public static function is_enable_page_preloader() {
+		return '1' === self::get_option( 'show_preloader' );
+	}
+}
