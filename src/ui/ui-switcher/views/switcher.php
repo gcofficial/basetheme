ab55394da32cafa06494ad397527db0f7d358632
Sergij Osadchy <sergijlys@gmail.com>
2016-02-12 22:14:26 +0200

    New styles for switcher

--- src/ui/ui-switcher/views/switcher.php
+++ src/ui/ui-switcher/views/switcher.php
@@ -11,21 +11,19 @@
 ?>
 
 <div {{ $attributes }}>
-	<input type="radio" name="{{ $name }}" id="{{ $name }}-{{ $value_first['key'] }}" value="{{ $value_first['key'] }}" 
-		@if( $value_first['key'] == $default )
-		checked="checked"
-		@endif
-	>
-	<label for="{{ $name }}-{{ $value_second['key'] }}" class="on">
-		{{ $value_first['value'] }}
-	</label>
+    <input type="radio" name="{{ $name }}" id="{{ $name }}-{{ $value_first['key'] }}" value="{{ $value_first['key'] }}"
+          @if( $value_first['key'] == $default )
+          checked="checked"
+          @endif
+      />
+    <label class="on" for="{{ $name }}-{{ $value_second['key'] }}"></label>
 
-	<input type="radio" name="{{ $name }}" id="{{ $name }}-{{ $value_second['key'] }}" value="{{ $value_second['key'] }}" 
-		@if( $value_second['key'] == $default )
-		checked="checked"
-		@endif
-	>
-	<label for="{{ $name }}-{{ $value_first['key'] }}" class="off">
-		{{ $value_second['value'] }}
-	</label>
+    <input type="radio" name="{{ $name }}" id="{{ $name }}-{{ $value_second['key'] }}" value="{{ $value_second['key'] }}"
+          @if( $value_second['key'] == $default )
+          checked="checked"
+          @endif
+      />
+    <label class="off" for="{{ $name }}-{{ $value_first['key'] }}"></label>
 </div>
+
+
